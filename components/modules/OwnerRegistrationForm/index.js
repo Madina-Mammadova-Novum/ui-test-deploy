0f7042adc190712e7d81c415ecf9c0335c255a71
Maksym Lukianov <maximssu@gmail.com>
2023-03-06 21:25:37 +0200

    refactor: signup page - personal data

---
+++ components/modules/OwnerRegistrationForm/index.js
@@ -0,0 +1,77 @@
+'use client';
+
+import { FormProvider, useForm } from 'react-hook-form';
+
+// import { useCallback } from 'react';
+// import { useDispatch } from 'react-redux';
+
+// import { signupSumbitAdapter } from '@/adapters/signupSubmitAdapter';
+import { Button } from '@/elements';
+import { resetPassword } from '@/services/user';
+import { PersonalDetails, Step } from '@/units';
+import { successToast } from '@/utils/hooks';
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import { signupSubmit } from '@/services/signup';
+// import { setRules, setTankers } from '@/store/entities/signup/slice';
+// import { useSignupSelector } from '@/store/selectors';
+// import { CompanyAddresess, CompanyDetails, PersonalDetails, SlotsDetails, TermsAndConditions } from '@/ui';
+// import { useHookForm } from '@/utils/hooks';
+
+const OwnerRegistrationForm = () => {
+  const methods = useForm({
+    // resolver: yupResolver(schema),
+  });
+  const onSubmit = async (data) => {
+    console.log({ data });
+    const { message } = await resetPassword({ data });
+    successToast(message, 'Some description');
+    methods.reset();
+  };
+  // const dispatch = useDispatch();
+  // const { rules, role, sameAddress } = useSignupSelector();
+  // const [signup] = signupSubmit();
+  // const { watch, reset, handleSubmit } = useHookForm();
+
+  // const slots = watch('slots.count');
+
+  // const handleRules = useCallback(() => {
+  //   dispatch(setRules(!rules));
+  // }, [dispatch, rules]);
+  //
+  // const handleSlots = useCallback(() => {
+  //   dispatch(setTankers([slots]));
+  // }, [dispatch, slots]);
+
+  // const onSubmit = async (data) => {
+  //   const result = signupSumbitAdapter(role, data, sameAddress);
+  //   await signup({ role, ...result }).unwrap();
+  //   reset();
+  // };
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <hr className="divide" />
+        <Step title="Step #2: Personal details" containerClass="flex flex-col py-5 gap-5">
+          <PersonalDetails />
+        </Step>
+        <Button
+          type="submit"
+          buttonProps={{
+            text: isSubmitting ? 'Please wait...' : 'Create account',
+            variant: isSubmitting ? 'secondary' : 'primary',
+            size: 'large',
+          }}
+          disabled={isSubmitting}
+          customStyles="mt-4 w-full"
+        />
+      </form>
+    </FormProvider>
+  );
+};
+
+export default OwnerRegistrationForm;
---
+++ components/units/PersonalDetails/index.js
@@ -0,0 +1,65 @@
+// import { Input, PhoneInput } from '@/elements';
+import { useFormContext } from 'react-hook-form';
+
+import { Input, PhoneInput } from '@/elements';
+import { PasswordValidation } from '@/units';
+// import { useHookForm } from '@/utils/hooks';
+
+const PersonalDetails = () => {
+  const {
+    register,
+    formState: { errors, isSubmitting },
+  } = useFormContext();
+
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <Input
+          {...register('firstName')}
+          label="First name"
+          placeholder="John"
+          error={errors.firstName?.message}
+          disabled={isSubmitting}
+        />
+        <Input
+          {...register('lastName')}
+          label="Last name"
+          placeholder="Doe"
+          error={errors.lastName?.message}
+          disabled={isSubmitting}
+        />
+        <Input
+          {...register('email')}
+          label="Email"
+          placeholder="Enter your email"
+          type="email"
+          error={errors.email?.message}
+          disabled={isSubmitting}
+        />
+      </div>
+      <div className="flex flex-col gap-5">
+        <p className="text-black w- font-semibold b-l text-sm pt-5">Provide contact phone numbers to contact you</p>
+        <div className="grid gap-5 grid-cols-1 md:grid-cols-2">
+          <PhoneInput
+            {...register('primaryPhoneNumber')}
+            label="Primary phone number"
+            disabled={isSubmitting}
+            error={errors.primaryPhoneNumber?.message}
+          />
+          <PhoneInput
+            {...register('secondaryPhoneNumber')}
+            label="Secondary phone number"
+            disabled={isSubmitting}
+            error={errors.secondaryPhoneNumber?.message}
+          />
+        </div>
+        <div>
+          <p className="text-black font-semibold text-sm py-5">Enter a strong password according to our requirements</p>
+          <PasswordValidation />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PersonalDetails;
