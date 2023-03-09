631fd3cefc957f520aea1e8003eb0e8781a28d66
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2023-03-09 16:24:22 +0200

    feat: form-manager-refactor

--- components/modules/LoginForm/index.js
+++ components/modules/LoginForm/index.js
@@ -1,45 +1,58 @@
 'use client';
 
-import { Form, Input, NextLink, PasswordInput } from '@/elements';
-import { useHookForm } from '@/utils/hooks';
+import { FormProvider, useForm } from 'react-hook-form';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+import { FormManager } from '@/common';
+import { Input, NextLink, PasswordInput } from '@/elements';
+import { emailSchema, passwordSchema } from '@/lib/schemas';
+
+const schema = yup
+  .object({
+    email: emailSchema().required(),
+    password: passwordSchema(),
+  })
+  .required();
 
 const LoginForm = () => {
-  const { register, handleSubmit, reset, formState } = useHookForm();
-  const { errors, isSubmitting } = formState;
+  const methods = useForm({
+    resolver: yupResolver(schema),
+  });
 
-  const onSubmit = async (data) => {
-    reset();
-    return data;
+  const {
+    register,
+    formState: { errors },
+  } = methods;
+
+  const onSubmit = (data) => {
+    console.log('data: ', data);
   };
 
   return (
-    <Form
-      className="flex flex-col gap-4"
-      onSubmit={handleSubmit(onSubmit)}
-      disabled={isSubmitting}
-      ctaProps={{ text: 'Log in', variant: 'primary', size: 'large' }}
-    >
-      <Input
-        {...register('email')}
-        type="email"
-        label="email"
-        placeholder="Enter your email"
-        error={errors?.email?.message}
-        required
-      />
-      <PasswordInput
-        {...register('password')}
-        label="password"
-        placeholder="Enter your password"
-        error={errors?.password?.message}
-        required
-      />
+    <FormProvider {...methods}>
+      <FormManager submitAction={onSubmit} submitProps={{ text: 'Log in', variant: 'primary', size: 'large' }}>
+        <Input
+          {...register('email')}
+          type="email"
+          label="email"
+          placeholder="Enter your email"
+          error={errors?.email?.message}
+        />
+        <PasswordInput
+          {...register('password')}
+          label="password"
+          placeholder="Enter your password"
+          error={errors?.password?.message}
+        />
+      </FormManager>
       <div className="ml-auto">
         <NextLink href="/forgot-password" className="text-blue underline text-xsm">
           Forgot your password
         </NextLink>
       </div>
-    </Form>
+    </FormProvider>
   );
 };
 
