f5f4a23d13632759feb96d4660588217943e6922
Maksym Lukianov <maximssu@gmail.com>
2023-03-13 22:16:15 +0200

    refactor: auth wrapper

--- components/modules/AuthWrapper/index.js
+++ components/modules/AuthWrapper/index.js
@@ -6,7 +6,7 @@ const AuthWrapper = ({ title, subtitle, children, containerClass }) => {
   return (
     <div className={`grid-start-1 3sm:col-start-2 m-auto ${containerClass}`}>
       <Title component="h2">{title}</Title>
-      <p className="pt-5 text-xsm text-black max-w-[296px]">{subtitle}</p>
+      {subtitle ?? <p className="pt-5 text-xsm text-black max-w-[296px]">{subtitle}</p>}
       {children}
     </div>
   );
@@ -14,11 +14,12 @@ const AuthWrapper = ({ title, subtitle, children, containerClass }) => {
 
 AuthWrapper.defaultProps = {
   containerClass: 'w-full',
+  subtitle: null,
 };
 
 AuthWrapper.propTypes = {
   title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
   containerClass: PropTypes.string,
 };
 
