70db596fe97404cef86e849e843644ab6d9b6953
Maksym Lukianov <maximssu@gmail.com>
2023-03-24 22:47:54 +0200

    refactor: block manager

--- components/common/BlockManager/index.js
+++ components/common/BlockManager/index.js
@@ -2,82 +2,23 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-import {
-  // BlockCompanyHistory,
-  BlockCta,
-  BlockCtaSingleImage,
-  // BlockCtaFeatured,
-  // BlockCtaImages,
-  // BlockFAQs,
-  // BlockHeroAnimatedWithTitle,
-  // BlockHeroImage,
-  // BlockHowItWorks,
-  // BlockImprovements,
-  // BlockJoinTeam,
-  // BlockPrivacyPolicy,
-  // BlockTeam,
-  // BlockTestimonials,
-  // BlockValues
-} from '@/blocks';
+import { BlockCta, BlockCtaSingleImage } from '@/blocks';
 import { makeId } from '@/utils/helpers';
 
-const getBlockComponent = ({ __component, ...rest }) => {
-  let Block;
-  switch (__component) {
-    case 'blocks.cta':
-      Block = BlockCta;
-      break;
-    case 'blocks.cta-single-image':
-      Block = BlockCtaSingleImage;
-      break;
-    // case 'blocks.single-how-it-works':
-    //   Block = BlockHowItWorks;
-    //   break;
-    // case 'blocks.single-why-we-are-better':
-    //   Block = BlockValues;
-    //   break;
-    // case 'blocks.improvement-process':
-    //   Block = BlockImprovements;
-    //   break;
-    // case 'blocks.cta-featured':
-    //   Block = BlockCtaFeatured;
-    //   break;
-    // case 'blocks.hero-animated-title':
-    //   Block = BlockHeroAnimatedWithTitle;
-    //   break;
-    // case 'blocks.cta-images':
-    //   Block = BlockCtaImages;
-    //   break;
-    // case 'blocks.testimonials':
-    //   Block = BlockTestimonials;
-    //   break;
-    // case 'blocks.team':
-    //   Block = BlockTeam;
-    //   break;
-    // case 'blocks.faq-questions':
-    //   Block = BlockFAQs;
-    //   break;
-    // case 'blocks.cta-join-team':
-    //   Block = BlockJoinTeam;
-    //   break;
-    // case 'blocks.hero-image':
-    //   Block = BlockHeroImage;
-    //   break;
-    // case 'blocks.company-history':
-    //   Block = BlockCompanyHistory;
-    //   break;
-    // case 'blocks.privacy-policy':
-    //   Block = BlockPrivacyPolicy;
-    //   break;
-    default:
-      return null;
-  }
-
-  return Block ? <Block key={makeId()} {...rest} /> : null;
+const BLOCK_COMPONENTS = {
+  'blocks.cta': BlockCta,
+  'blocks.cta-single-image': BlockCtaSingleImage,
 };
 
 const BlockManager = ({ blocks }) => {
-  return blocks.map(getBlockComponent);
+  if (!blocks) return null;
+
+  return blocks.map((block) => {
+    const Block = BLOCK_COMPONENTS[block.__component];
+    if (!Block) return null;
+
+    return <Block key={makeId()} {...block} />;
+  });
 };
 
 BlockManager.defaultProps = {
@@ -89,9 +30,9 @@ BlockManager.propTypes = {
     PropTypes.shape({
       id: PropTypes.number.isRequired,
       __component: PropTypes.string.isRequired,
-      title: PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.oneOf([null]).isRequired]),
-      subTitle: PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.oneOf([null]).isRequired]),
-      shortDescription: PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.oneOf([null]).isRequired]),
+      title: PropTypes.string,
+      subTitle: PropTypes.string,
+      shortDescription: PropTypes.string,
     })
   ),
 };
