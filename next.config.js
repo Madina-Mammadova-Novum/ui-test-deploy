8c22dee964b9461c65ab681a66577bcb460651db
Maksym Lukianov <maximssu@gmail.com>
2023-04-04 22:08:18 +0300

    feat: added additional domain to next config

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     unoptimized: false, // true for `yarn export`
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60,
-    domains: ['localhost'],
+    domains: ['localhost', 'shiplink.oyihost.com'],
     deviceSizes: [340, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [20, 21, 24, 37, 40, 67, 77, 140, 160, 280, 320, 549, 557, 558, 865, 1920],
   },
