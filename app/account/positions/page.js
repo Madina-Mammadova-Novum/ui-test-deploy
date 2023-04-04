05a2b50c5558b6126efacb7a82617cbbcdccc99f
Maksym Lukianov <maximssu@gmail.com>
2023-04-04 18:46:15 +0300

    fix: build process

--- app/account/positions/page.js
+++ app/account/positions/page.js
@@ -1,6 +1,5 @@
 import { metaData } from '@/adapters/metaData';
 import { AccountPositions, AccountWrapper } from '@/modules';
-import { getUserPositions } from '@/services';
 
 export async function generateMetadata() {
   return metaData({
@@ -13,11 +12,9 @@ export async function generateMetadata() {
 }
 
 export default async function AccountPostions() {
-  const data = await getUserPositions();
-
   return (
     <AccountWrapper title="My positions" containerClass="grow px-5">
-      <AccountPositions data={data} containerClass="flex flex-col gap-2" />
+      <AccountPositions containerClass="flex flex-col gap-2" />
     </AccountWrapper>
   );
 }
