671e4864abb3b70841d2cc36574dee830fa2abe3
Yevheniia Nikolska <yevheniia.nikolska@otakoyi.com>
2023-04-06 17:32:21 +0300

    fix: delete extra code

--- components/blocks/CategoryList/adapter.js
+++
@@ -1,8 +0,0 @@
-export const updateCategoryList = async (block) => {
-  const { categories } = block;
-  const { data } = categories;
-  block.categories = data.map((category) => {
-    return category;
-  });
-  return block;
-};
