3420c4997067d1b95f69dd505a310ff989c304b9
Yevheniia Nikolska <yevheniia.nikolska@otakoyi.com>
2023-04-18 17:49:51 +0300

    fix: faq-block first question opened, style-fix

--- components/blocks/FAQBlock/index.js
+++ components/blocks/FAQBlock/index.js
@@ -10,7 +10,7 @@ import { categoryPropTypes, ctaPropTypes } from '@/lib/types';
 import { Accordion, AccordionCTA, TabsAsLinks } from '@/units';
 
 const FAQBlock = ({ title, subTitle, shortDescription, items, categories, category, cta }) => {
-  const [open, setOpen] = useState(1);
+  const [open, setOpen] = useState(null);
   const handleOpen = (value) => {
     setOpen(open === value ? 0 : value);
   };
@@ -30,7 +30,7 @@ const FAQBlock = ({ title, subTitle, shortDescription, items, categories, catego
         {subTitle && <div>{subTitle}</div>}
         {shortDescription && <div>{shortDescription}</div>}
         <TabsAsLinks tabs={tabs} activeTab={category.id} />
-        <div className="rounded-[10px] pt-1.5 px-5 pb-5 bg-white shadow-xmd divide-y divide-gray-darker mt-1">
+        <div className="relative rounded-[10px] pt-1.5 px-5 pb-5 bg-white divide-y divide-gray-darker mt-1">
           {items &&
             items.map(({ id, answer, question }) => (
               <Accordion
@@ -49,6 +49,7 @@ const FAQBlock = ({ title, subTitle, shortDescription, items, categories, catego
           <div className="text-black pt-5">
             {cta && <AccordionCTA shortDescription={cta.shortDescription} title={cta.title} buttons={cta.buttons} />}
           </div>
+          <div className="rounded-[10px]  h-[calc(100%_-_135px)] w-full absolute shadow-xmd bottom-0 left-0 -z-10" />
         </div>
       </div>
     </section>
