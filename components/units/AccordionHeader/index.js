06af44500f65018474087b137c41dfcdec54c34f
Yevheniia Nikolska <yevheniia.nikolska@otakoyi.com>
2023-04-18 17:51:44 +0300

    fix: eslint prettify

--- components/units/AccordionHeader/index.js
+++ components/units/AccordionHeader/index.js
@@ -10,13 +10,16 @@ const AccordionHeader = ({ children, onClick, isActive, isFullWidth }) => {
   return (
     <div
       aria-hidden
-      className={classnames(!isFullWidth && 'px-[30px]', 'flex gap-x-5 justify-between pb-2.5 pt-5 w-full cursor-pointer')}
+      className={classnames(
+        !isFullWidth && 'px-[30px]',
+        'flex gap-x-5 justify-between pb-2.5 pt-5 w-full cursor-pointer'
+      )}
       onClick={onClick}
     >
       <Title level={isFullWidth ? '3' : '2'} className="text-black">
         {children}
       </Title>
-      <div className="shrink-0" >
+      <div className="shrink-0">
         {isActive ? <MinusIcon width={24} height={24} /> : <PlusIcon width={24} height={24} />}
       </div>
     </div>
