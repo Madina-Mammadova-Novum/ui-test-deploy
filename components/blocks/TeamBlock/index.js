592e79483b5d60c750e0fb1c44f8a3dcd68189af
Yevheniia Nikolska <yevheniia.nikolska@otakoyi.com>
2023-04-25 13:52:19 +0300

    refactor: rewhrite team-block using prints

--- components/blocks/TeamBlock/index.js
+++ components/blocks/TeamBlock/index.js
@@ -11,6 +11,52 @@ import { getStrapiMedia } from '@/utils';
 import { makeId } from '@/utils/helpers';
 
 const TeamBlock = ({ title, subTitle, shortDescription, members }) => {
+
+  const printSocialLink = (link) => (
+    <NextLink key={makeId()} href={delve(link, 'path')} title={delve(link, 'title')}>
+      <HoverableIcon
+        className="border border-gray-darker rounded-md"
+        icon={
+          <NextImage
+            alt={delve(link, 'title')}
+            src={getStrapiMedia(delve(link, 'coverImage.format.original.url'), '')}
+            height={20}
+            width={20}
+          />
+        }
+      />
+    </NextLink>
+  );
+
+  const printMember = ({ fullName, content, position, coverImage, socialLinks }) => (
+    <div
+      key={makeId()}
+      className="flex items-center flex-col mt-[60px] text-black shadow px-[30px] pb-[30px] bg-white rounded-base"
+    >
+      {coverImage && (
+        <NextImage
+          alt={delve(coverImage, 'alternativeText')}
+          src={getStrapiMedia(delve(coverImage, 'format.original.url'), '')}
+          height={120}
+          width={120}
+          className="h-[120px] w-[120px] object-cover object-center rounded-full mb-2.5 -mt-[60px]"
+        />
+      )}
+      {fullName && (
+        <Title level="2" className="mb-1">
+          {fullName}
+        </Title>
+      )}
+      {position && (
+        <Title level="4" className="font-semibold mb-1">
+          {delve(position, 'title')}
+        </Title>
+      )}
+      {socialLinks && <div className="flex gap-x-2.5 mb-2.5">{socialLinks.map(printSocialLink)}</div>}
+      {content && <div className="text-xsm">{parse(content)}</div>}
+    </div>
+  );
+
   return (
     <section>
       <div className="container mx-auto px-[54px] max-w-[1258px]">
@@ -21,60 +67,7 @@ const TeamBlock = ({ title, subTitle, shortDescription, members }) => {
         )}
         {subTitle && subTitle}
         {shortDescription && shortDescription}
-        {members ? (
-          <div className="grid grid-cols-3 gap-5">
-            {members.map(({ fullName, content, position, coverImage, socialLinks }) => {
-              return (
-                <div
-                  key={makeId()}
-                  className="flex items-center flex-col mt-[60px] text-black shadow px-[30px] pb-[30px] bg-white rounded-base"
-                >
-                  {coverImage && (
-                    <NextImage
-                      alt={delve(coverImage, 'alternativeText')}
-                      src={getStrapiMedia(delve(coverImage, 'format.original.url'), '')}
-                      height={120}
-                      width={120}
-                      className="h-[120px] w-[120px] object-cover object-center rounded-full mb-2.5 -mt-[60px]"
-                    />
-                  )}
-                  {fullName && (
-                    <Title level="2" className="mb-1">
-                      {fullName}
-                    </Title>
-                  )}
-                  {position && (
-                    <Title level="4" className="font-semibold mb-1">
-                      {delve(position, 'title')}
-                    </Title>
-                  )}
-                  {socialLinks && (
-                    <div className="flex gap-x-2.5 mb-2.5">
-                      {socialLinks.map((link) => {
-                        return (
-                          <NextLink key={makeId()} href={delve(link, 'path')} title={delve(link, 'title')}>
-                            <HoverableIcon
-                              className="border border-gray-darker rounded-md"
-                              icon={
-                                <NextImage
-                                  alt={delve(link, 'title')}
-                                  src={getStrapiMedia(delve(link, 'coverImage.format.original.url'), '')}
-                                  height={20}
-                                  width={20}
-                                />
-                              }
-                            />
-                          </NextLink>
-                        );
-                      })}
-                    </div>
-                  )}
-                  {content && <div className="text-xsm">{parse(content)}</div>}
-                </div>
-              );
-            })}
-          </div>
-        ) : null}
+        {members ? <div className="grid grid-cols-3 gap-5">{members.map(printMember)}</div> : null}
       </div>
     </section>
   );
