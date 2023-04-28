a73daee68042b5e308c2f0ebb271ccfdb2cceb57
Yevheniia Nikolska <yevheniia.nikolska@otakoyi.com>
2023-04-28 13:34:06 +0300

    fix: link color in faq block

--- components/elements/AccordionBody/index.js
+++ components/elements/AccordionBody/index.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 
 const AccordionBody = ({ children, isFullWidth }) => {
   return (
-    <div className={classnames(!isFullWidth && 'pl-[30px]', 'pb-4 pr-[74px] content-wrapper space-y-2.5')}>
+    <div className={classnames(isFullWidth ? 'content-wrapper' : 'pl-[30px]', 'pb-4 pr-[74px] space-y-2.5')}>
       {children}
     </div>
   );
