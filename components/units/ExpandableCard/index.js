855563271df33ef765a9e35a9e32a77c331c94c2
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2023-05-03 18:57:24 +0300

    fix: added expand/collapse all cards action

--- components/units/ExpandableCard/index.js
+++ components/units/ExpandableCard/index.js
@@ -4,12 +4,16 @@ import { fleetsHeaderDataAdapter, fleetsRowsDataAdapter } from '@/adapters';
 import { ExpandableCardHeader, ExpandableCardWrapper, Table } from '@/elements';
 import { fleetsHeader } from '@/utils/mock';
 
-const ExpandableCard = ({ data, className }) => {
+const ExpandableCard = ({ data, expandAll, className }) => {
   const headerData = fleetsHeaderDataAdapter({ data });
   const rowsData = fleetsRowsDataAdapter({ data: data.tankers });
 
   return (
-    <ExpandableCardWrapper className={className} headerComponent={<ExpandableCardHeader headerData={headerData} />}>
+    <ExpandableCardWrapper
+      className={className}
+      expandAll={expandAll}
+      headerComponent={<ExpandableCardHeader headerData={headerData} />}
+    >
       <Table headerData={fleetsHeader} rows={rowsData} />
     </ExpandableCardWrapper>
   );
