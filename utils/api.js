b2826fe73455aa54f95e0445166e99f47451ae6c
Maksym Lukianov <maximssu@gmail.com>
2023-05-12 10:21:36 +0300

    feat(api-handler): removed unused functions from api handler

--- utils/api.js
+++ utils/api.js
@@ -1,28 +1,5 @@
 import { responseAdapter } from '@/adapters/response';
 import { SYSTEM_ERROR } from '@/lib/constants';
-import { getApiURL, getIdentityApiURL, getStrapiURL } from '@/utils/index';
-
-export const endpointPath = (path, provider) => {
-  let apiURL = '';
-  switch (provider) {
-    case 'identify': {
-      apiURL = getIdentityApiURL(path);
-      break;
-    }
-    case 'backend': {
-      apiURL = getApiURL(path);
-      break;
-    }
-    case 'strapi': {
-      apiURL = getStrapiURL(path);
-      break;
-    }
-    default: {
-      apiURL = null;
-    }
-  }
-  return apiURL;
-};
 
 export const externalErrorHandler = (status, message, errors = []) => {
   const statusMessage = message === undefined || message === null ? SYSTEM_ERROR : message;
@@ -78,41 +55,4 @@ export const externalApiHandler = async (options) => {
     console.error(error);
     return externalErrorHandler(500, 'External server error');
   }
-};
-
-export const postHandler = (path, body, provider) => {
-  return externalApiHandler({
-    endpoint: endpointPath(path, provider),
-    requestMethod: 'POST',
-    body,
-  });
-};
-
-export const putHandler = (path, body, provider) => {
-  return externalApiHandler({
-    endpoint: endpointPath(path, provider),
-    requestMethod: 'PUT',
-    body,
-  });
-};
-
-export const patchHandler = (path, body, provider) => {
-  return externalApiHandler({
-    endpoint: endpointPath(path, provider),
-    requestMethod: 'PATCH',
-    body,
-  });
-};
-
-/**
- *
- * @param path
- * @param provider
- * @returns {Promise<{data?: *, error: null|{message: null|string|*, errors: null|*[]}, status: *}|{message: null|string|*, errors: null|*[]}|undefined>}
- */
-export const getHandler = (path, provider) => {
-  return externalApiHandler({
-    endpoint: endpointPath(path, provider),
-    requestMethod: 'GET',
-  });
 };
