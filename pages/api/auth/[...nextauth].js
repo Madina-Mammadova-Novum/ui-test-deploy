c14c2e333822e95d0db47fc3d87f167f9140a4c0
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2023-05-15 13:16:20 +0300

    fix: removed-auth-file

--- pages/api/auth/[...nextauth].js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import NextAuth from 'next-auth';
-
-export const authOptions = {
-  providers: [],
-};
-export default NextAuth(authOptions);
