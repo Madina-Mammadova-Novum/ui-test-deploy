d6f910fdc41e9fe7c545821723757e4fc5b4c398
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2023-05-15 11:36:24 +0300

    fix: build-issues

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import NextAuth from 'next-auth';
+
+export const authOptions = {
+  providers: [],
+};
+export default NextAuth(authOptions);
