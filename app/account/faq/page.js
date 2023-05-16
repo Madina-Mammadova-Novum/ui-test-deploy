9a32fba4c0f6aa6a4a9db9933f898a3926f34a6c
Yevheniia Nikolska <yevheniia.nikolska@otakoyi.com>
2023-05-16 13:38:11 +0300

    refactor: delete commented code

--- app/account/faq/page.js
+++ app/account/faq/page.js
@@ -15,18 +15,8 @@ export function generateMetadata() {
 }
 
 const Faq = () => {
-  // const blocks = [
-  //   {
-  //     "id": 4,
-  //     "__component": "blocks.faq-block",
-  //     "title": null,
-  //     "shortDescription": null,
-  //     "subTitle": null
-  //   },
-  // ]
   return (
     <AccountWrapper containerClass="grow px-5">
-      {/* <div className="space-y-[100px]">{blocks && <BlockManager blocks={blocks} />}</div> */}
       <FAQByType />
     </AccountWrapper>
   );
