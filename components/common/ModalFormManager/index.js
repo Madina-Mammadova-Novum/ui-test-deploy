2c023c244043840153e612191ea151bfd915b5bd
Yevheniia Nikolska <yevheniia.nikolska@otakoyi.com>
2023-05-19 10:24:33 +0300

    refactor: eslint-prettify

--- components/common/ModalFormManager/index.js
+++ components/common/ModalFormManager/index.js
@@ -7,38 +7,40 @@ import { ModalFormManagerPropTypes } from '@/lib/types';
 import { Button } from '@/elements';
 
 const ModalFormManager = ({ onClose, children, submitAction, submitButton, className = '', specialStyle }) => {
-    const {
-        handleSubmit,
-        formState: { isSubmitting },
-    } = useFormContext();
-    const { text, variant, size, disabled, className: buttonClassName } = submitButton;
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useFormContext();
+  const { text, variant, size, disabled, className: buttonClassName } = submitButton;
 
-    return (
-        <form className={`${className} flex flex-col gap-5`} onSubmit={handleSubmit(submitAction)}>
-            {children}
-            <div className={`flex ${specialStyle ? 'justify-between items-start' : "justify-end"} gap-x-2.5 whitespace-nowrap`}>
-                <Button
-                    onClick={onClose}
-                    customStyles="w-[100%]"
-                    customStylesFromWrap={specialStyle && `flex-1`}
-                    buttonProps={{ text: 'Cancel', variant: 'tertiary', size: 'large' }}
-                />
-                <Button
-                    type="submit"
-                    buttonProps={{
-                        text: isSubmitting ? 'Please wait...' : text,
-                        variant: isSubmitting ? 'secondary' : variant,
-                        size,
-                    }}
-                    disabled={disabled || isSubmitting}
-                    customStyles={`${buttonClassName} w-full`}
-                    customStylesFromWrap={specialStyle && `flex-1`}
-                />
-            </div>
-        </form >
-    );
+  return (
+    <form className={`${className} flex flex-col gap-5`} onSubmit={handleSubmit(submitAction)}>
+      {children}
+      <div
+        className={`flex ${specialStyle ? 'justify-between items-start' : 'justify-end'} gap-x-2.5 whitespace-nowrap`}
+      >
+        <Button
+          onClick={onClose}
+          customStyles="w-[100%]"
+          customStylesFromWrap={specialStyle && `flex-1`}
+          buttonProps={{ text: 'Cancel', variant: 'tertiary', size: 'large' }}
+        />
+        <Button
+          type="submit"
+          buttonProps={{
+            text: isSubmitting ? 'Please wait...' : text,
+            variant: isSubmitting ? 'secondary' : variant,
+            size,
+          }}
+          disabled={disabled || isSubmitting}
+          customStyles={`${buttonClassName} w-full`}
+          customStylesFromWrap={specialStyle && `flex-1`}
+        />
+      </div>
+    </form>
+  );
 };
 
 ModalFormManager.propTypes = ModalFormManagerPropTypes;
 
-export default ModalFormManager
+export default ModalFormManager;
