8f97466c8b742b551b2a2b8bc34d8b3ad2d82867
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2023-05-24 20:21:39 +0300

    fix: hotfix

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,6 @@ export const metadata = {
   },
 };
 
-export default async function RootLayout({ children }) {
+export default function RootLayout({ children }) {
   return <AuthManager>{children}</AuthManager>;
 }
