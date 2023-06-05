8beaf82a3cdf459f73956db2ea982d9ca5822aa2
Yevheniia Nikolska <yevheniia.nikolska@otakoyi.com>
2023-06-05 12:56:22 +0300

    feat: add scroll to how-it-works

--- components/blocks/HomeSearchBlock/index.js
+++ components/blocks/HomeSearchBlock/index.js
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 
 import PropTypes from 'prop-types';
 
+import ArrowSVG from '@/assets/images/arrow.svg';
+import { Button } from '@/elements';
 import { AccountTools, TankerSearch } from '@/modules';
 import { Tabs } from '@/units';
 
@@ -26,6 +28,11 @@ const HomeSearchBlock = ({ title, subTitle, shortDescription }) => {
     setActiveTab(value);
   };
 
+  const handleClickScroll = () => {
+    const elementToScroll = document.getElementById('how-it-works');
+    elementToScroll?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   const handleViewType = (typeOfView) => {
     switch (typeOfView) {
       case 'Tools':
@@ -40,7 +47,17 @@ const HomeSearchBlock = ({ title, subTitle, shortDescription }) => {
       {title && <div>{title}</div>}
       {subTitle && <div>{subTitle}</div>}
       {shortDescription && <div>{shortDescription}</div>}
-      <div className="container mx-auto px-[54px] max-w-[1258px] -mt-[175px]">
+      <div className="container mx-auto px-[54px] max-w-[1258px] -mt-[205px]">
+        <Button
+          buttonProps={{
+            text: 'How it works',
+            variant: 'primary',
+            size: 'large',
+            icon: { after: <ArrowSVG viewBox="0 0 24 24" className="fill-white w-5 h-5" /> },
+          }}
+          customStyles="!bg-transparent !p-0 font-medium self-baseline mb-2"
+          onClick={handleClickScroll}
+        />
         <Tabs tabs={tabs} activeTab={activeTab} onClick={handleActiveTab} customStyles="mb-1" />
         {handleViewType(activeTab)}
       </div>
