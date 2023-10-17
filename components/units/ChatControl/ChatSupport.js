21171801ce444bfad3fe02fd5c4bab93156ee849
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2023-10-18 00:59:34 +0300

    fix: unreaded-message-counter for support chat

--- components/units/ChatControl/ChatSupport.js
+++ components/units/ChatControl/ChatSupport.js
@@ -1,31 +1,27 @@
 'use client';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { ChatSupportPropTypes } from '@/lib/types';
 
 import SupportSVG from '@/assets/images/support.svg';
-import { Title } from '@/elements';
+import { Badge, Title } from '@/elements';
 import { chatService } from '@/services/signalR';
-import { resetUser, setUser } from '@/store/entities/chat/slice';
 import { getChatSelector } from '@/store/selectors';
 
 const ChatSupport = ({ title, description }) => {
-  const dispatch = useDispatch();
-
   const { support } = useSelector(getChatSelector);
 
   const handleOpenConversation = (e) => {
     e.stopPropagation();
-    dispatch(resetUser());
 
-    chatService.initChat({ chatId: support?.chatId });
-    dispatch(setUser(support));
+    chatService.initChat({ chatId: support?.chatId, vessel: support });
   };
 
   return (
     <div aria-hidden onClick={handleOpenConversation}>
-      <div className="text-black flex items-center gap-x-3 cursor-pointer">
+      <div className="text-black relative flex items-center gap-x-3 cursor-pointer">
+        <Badge counter={support?.unreadedMessages} />
         <div className="w-0.5 h-10 rounded-xl bg-blue" />
         <SupportSVG />
         <div className="flex flex-col  w-5/6">
