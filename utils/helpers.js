95a949e135e55c03fce4ab6e5355c13291a1e6a9
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2023-11-03 18:39:53 +0200

    fix: build-issue

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,7 +3,6 @@
 import { HttpTransportType } from '@microsoft/signalr';
 import parse from 'html-react-parser';
 import dynamic from 'next/dynamic';
-import { getSession } from 'next-auth/react';
 
 import { transformDate } from './date';
 
@@ -572,12 +571,3 @@ export function checkTankerStatus(date) {
 
   return today > openDate;
 }
-
-export const sessionValidity = async () => {
-  const session = await getSession();
-
-  const isExpired = session?.expires <= Date.now();
-  const isValid = session?.accessToken !== undefined && !isExpired;
-
-  return { isValid, isExpired };
-};
