eecc8b2f0b3cb63b8ad80deb45ba6725288aafe5
Serh1yPlekan <sergiy.plekan@vaimo.com>
2023-11-23 15:35:55 +0200

    feat: tools pathbuilding setup

--- services/tools/index.js
+++ services/tools/index.js
@@ -10,3 +10,23 @@ export const getEstimation = async ({ data }) => {
     ...response,
   };
 };
+
+export const getTransitionalCoordinates = async () => {
+  const body = [
+    {
+      StartLon: 0,
+      StartLat: 0,
+      StartPortCode: 'RUNVS',
+      EndLon: 0,
+      EndLat: 0,
+      EndPortCode: 'BGBOJ',
+      GreatCircleInterval: 0,
+      AllowedAreas: [2, 5, 7, 14, 10003],
+      SecaAvoidance: 0,
+      AslCompliance: 0,
+    },
+  ];
+
+  const response = await postData(`tools/get-transitional-coordinates`, body);
+  return response;
+};
