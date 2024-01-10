4f85c037ff004c916f9eecc5c32fc786fa057bbe
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2024-01-10 02:31:47 +0200

    fix: page max width screen

--- components/modules/AccountContainer/index.js
+++ components/modules/AccountContainer/index.js
@@ -35,9 +35,7 @@ export default function AccountContainer({ children }) {
 
   return (
     <div
-      className={`flex flex-col bg-gray-light min-h-screen max-w-screen-2xl grow ${
-        collapsed ? 'ml-16' : 'ml-16 lg:ml-64'
-      }`}
+      className={`flex flex-col bg-gray-light min-h-screen max-w-screen grow ${collapsed ? 'ml-16' : 'ml-16 lg:ml-64'}`}
     >
       {children}
     </div>
