0d9f0f862c9e1efd0e2889e67f729037858ac776
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2024-02-01 18:06:37 +0200

    fix: bugs-issues

--- components/elements/ReActivateButton/index.js
+++ components/elements/ReActivateButton/index.js
@@ -4,17 +4,15 @@ import { ArchiveButtonPropTypes } from '@/lib/types';
 
 import HistorySVG from '@/assets/images/history.svg';
 
-const ReActivateButton = ({ onClick, className }) => {
+const ReActivateButton = ({ onClick }) => {
   return (
-    <div className={`${className}`}>
-      <button
-        type="button"
-        onClick={onClick}
-        className="p-1 relative rounded-md border border-gray-light hover:bg-gray-darker outline-none bg-transparent"
-      >
-        <HistorySVG className="fill-black" />
-      </button>
-    </div>
+    <button
+      type="button"
+      onClick={onClick}
+      className="p-1 relative rounded-md border border-gray-light hover:bg-gray-darker outline-none bg-transparent"
+    >
+      <HistorySVG className="fill-black" />
+    </button>
   );
 };
 
--- components/units/ChatSession/index.js
+++ components/units/ChatSession/index.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMemo } from 'react';
+import { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { ChatSessionPropTypes } from '@/lib/types';
@@ -14,12 +14,15 @@ import { ChatConversationCard, ChatSubModal } from '@/units';
 
 const ChatSession = ({ data, tab, sessionId, setSessionId }) => {
   const dispatch = useDispatch();
+  const ref = useRef(null);
 
   const { user } = useSelector(getChatSelector).chats;
 
   const handleModal = (e, id) => {
     e.stopPropagation();
     setSessionId(id);
+
+    ref?.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   const handleAction = (e, key) => {
@@ -38,39 +41,36 @@ const ChatSession = ({ data, tab, sessionId, setSessionId }) => {
     setSessionId('');
   };
 
-  const handleOpenConversation = () => {
+  const handleOpenConversation = async () => {
     if (user.data?.chatId === data.chatId) return;
 
-    сhatSessionService.stop();
+    await сhatSessionService.stop();
 
     dispatch(removeCollapsedChat(data?.chatId));
     dispatch(setUser(data));
     dispatch(setConversation(true));
   };
 
-  const actions = useMemo(
-    () => ({
-      active: <ArchiveButton onClick={(e) => handleModal(e, data?.chatId)} />,
-      archived: <ReActivateButton onClick={(e) => handleModal(e, data?.chatId)} />,
-      deactivate: (
-        <ChatSubModal
-          tab={tab}
-          title="Do you want to archive this chat?"
-          onCancel={(e) => handleCancel(e)}
-          onClick={(e) => handleAction(e, 'deactivate')}
-        />
-      ),
-      reactivate: (
-        <ChatSubModal
-          tab={tab}
-          title="Do you want to restore this chat?"
-          onCancel={(e) => handleCancel(e)}
-          onClick={(e) => handleAction(e, 'reactivate')}
-        />
-      ),
-    }),
-    [tab, sessionId]
-  );
+  const actions = {
+    active: <ArchiveButton onClick={(e) => handleModal(e, data?.chatId)} />,
+    archived: <ReActivateButton onClick={(e) => handleModal(e, data?.chatId)} />,
+    deactivate: (
+      <ChatSubModal
+        tab={tab}
+        title="Do you want to archive this chat?"
+        onCancel={(e) => handleCancel(e)}
+        onClick={(e) => handleAction(e, 'deactivate')}
+      />
+    ),
+    reactivate: (
+      <ChatSubModal
+        tab={tab}
+        title="Do you want to restore this chat?"
+        onCancel={(e) => handleCancel(e)}
+        onClick={(e) => handleAction(e, 'reactivate')}
+      />
+    ),
+  };
 
   const state = {
     active: actions.deactivate,
@@ -80,9 +80,9 @@ const ChatSession = ({ data, tab, sessionId, setSessionId }) => {
   return (
     <div className="flex justify-between relative cursor-pointer" aria-hidden onClick={handleOpenConversation}>
       <ChatConversationCard data={data} />
-      <div className="flex flex-col relative justify-end overflow-x-hidden">
+      <div className="flex flex-col relative justify-end">
         <Badge className="h-5 w-5 -top-0.5 right-1 p-1" counter={data?.messageCount} />
-        <div>{actions[tab]}</div>
+        <div ref={ref}>{actions[tab]}</div>
         {sessionId === data?.chatId && state[tab]}
       </div>
     </div>
