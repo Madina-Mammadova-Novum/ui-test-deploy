44298e55a5dec2c5a6d9ac86f1e6e3b80c6a94d8
Yevhenii Alieksandrov <evhenii.aleksandrov@otakoyi.com>
2024-04-11 15:30:04 +0300

    fix: counter-validation

--- store/entities/offer/types.js
+++ store/entities/offer/types.js
@@ -1,4 +1,5 @@
 export const OFFER = {
   GET_OFFER_OPTIONS: 'offer/getOfferOptions',
   GET_OFFER_VALIDATION: 'offer/getOfferValidation',
+  GET_COUNTER_OFFER_VALIDATION: 'offer/getCounterOfferValidation',
 };
