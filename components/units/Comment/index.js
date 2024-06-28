4d0fd8fc416ce9375c2a49bac378bf2f7e129aba
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2024-06-28 19:25:57 +0300

    refactor: color logic updated for role on comment section

--- components/units/Comment/index.js
+++ components/units/Comment/index.js
@@ -11,16 +11,15 @@ const Comment = ({ title = '', date = '', time = '', sentBy = '' }) => {
 
   const { isCharterer, isOwner } = getRoleIdentity({ role });
 
-  const getSentByClass = ({ type = '' }) => {
-    if ((isCharterer && sentBy === 'Charterer') || (isOwner && sentBy === 'Owner')) {
-      return `${type}-green-600`;
-    }
-    return `${type}-blue-600`;
-  };
+  const borderColor =
+    (isCharterer && sentBy === 'Charterer') || (isOwner && sentBy === 'Owner') ? 'border-green-600' : 'border-blue-600';
+
+  const textColor =
+    (isCharterer && sentBy === 'Charterer') || (isOwner && sentBy === 'Owner') ? 'text-green-600' : 'text-blue-600';
 
   return title ? (
-    <div className={`p-2 border rounded-lg ${getSentByClass({ type: 'border' })}`}>
-      <Title level="2" className={`text-sm font-semibold ${getSentByClass({ type: 'text' })}`}>
+    <div className={`p-2 border rounded-lg ${borderColor}`}>
+      <Title level="2" className={`text-sm font-semibold ${textColor}`}>
         {sentBy}:
       </Title>
       <Title level="6" className="text-xsm font-semibold mt-2.5">
