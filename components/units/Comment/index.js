9e027da0afcb3bf178741037121a4aa88c58eace
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2024-06-28 19:10:32 +0300

    feat: comment on offer updated

--- components/units/Comment/index.js
+++ components/units/Comment/index.js
@@ -1,15 +1,33 @@
+import { useSelector } from 'react-redux';
+
 import { CommentPropTypes } from '@/lib/types';
 
 import { DateTimeRow, Title } from '@/elements';
+import { getUserDataSelector } from '@/store/selectors';
+import { getRoleIdentity } from '@/utils/helpers';
 
-const Comment = ({ title = '', date = '', time = '' }) => {
+const Comment = ({ title = '', date = '', time = '', sentBy = '' }) => {
+  const { role } = useSelector(getUserDataSelector);
+
+  const { isCharterer, isOwner } = getRoleIdentity({ role });
+
+  const getSentByClass = ({ type = '' }) => {
+    if ((isCharterer && sentBy === 'Charterer') || (isOwner && sentBy === 'Owner')) {
+      return `${type}-green-600`;
+    }
+    return `${type}-blue-600`;
+  };
+
   return title ? (
-    <>
+    <div className={`p-2 border rounded-lg ${getSentByClass({ type: 'border' })}`}>
+      <Title level="2" className={`text-sm font-semibold ${getSentByClass({ type: 'text' })}`}>
+        {sentBy}:
+      </Title>
       <Title level="6" className="text-xsm font-semibold mt-2.5">
         {title}
       </Title>
       <DateTimeRow date={date} time={time} />
-    </>
+    </div>
   ) : null;
 };
 
