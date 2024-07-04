7d8d64cbfd22144b1fbd775b943ab3e2316fb27d
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2024-07-04 11:40:17 +0300

    fix: bug493 button disabled logic fixed for expired deal

--- components/modules/OnSubs/index.js
+++ components/modules/OnSubs/index.js
@@ -40,7 +40,7 @@ const OnSubs = () => {
         expand={toggle}
         footer={
           <OnSubsExpandedFooter
-            underRecap={!rowData?.isCountdownActive}
+            underRecap={!rowData?.isCountdownActive || !!rowData?.expiresAt || !!rowData?.failedAt}
             identity={{ isOwner, isCharterer }}
             status={{ chraterer: rowData.chartererConfirmed, owner: rowData.ownerConfirmed }}
             offerId={rowData?.id}
