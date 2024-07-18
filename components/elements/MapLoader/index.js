451b748fa81c2392b11e748462f989d65bc0c62e
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2024-07-18 17:12:58 +0300

    feat: story 395  loader added for leaflet

---
+++ components/elements/MapLoader/index.js
@@ -0,0 +1,9 @@
+const MapLoader = () => {
+  return (
+    <div className="absolute w-full h-full top-0 left-0 z-[999] flex justify-center items-center bg-black opacity-50">
+      <div className="size-10 border-4 rounded-full border-t-blue animate-spin" />
+    </div>
+  );
+};
+
+export default MapLoader;
