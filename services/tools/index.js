268257f84e634494c315e9adefed022665bf6aad
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2024-08-06 19:16:23 +0300

    fix: bug663 corinth canal removed from list

--- services/tools/index.js
+++ services/tools/index.js
@@ -21,7 +21,7 @@ export const getTransitionalCoordinates = async ({ StartPortCode = '', EndPortCo
       EndLat: 0,
       EndPortCode,
       GreatCircleInterval: 0,
-      AllowedAreas: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 10004],
+      AllowedAreas: [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 10004],
       SecaAvoidance: 0,
       AslCompliance: 0,
     },
