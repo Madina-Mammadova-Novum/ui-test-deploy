9155ef1778000fac70870f8d3ae5a5ff094e90ec
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2024-09-02 20:16:19 +0300

    fix(851): close notification logic added after see details clicked

--- components/elements/NotificationAlert/index.js
+++ components/elements/NotificationAlert/index.js
@@ -9,7 +9,7 @@ const NotificationAlert = ({ handleClose, notificationData }) => {
   return (
     <div className="my-3 mb-0 ml-auto box-border flex w-max max-w-[31.875rem] justify-between gap-2.5 rounded-base border border-solid bg-gray-50 px-4 py-2.5">
       <div className="flex items-center gap-2.5">
-        <NotificationCard data={notificationData} useDivider={false} />
+        <NotificationCard data={notificationData} useDivider={false} handleClose={handleClose} />
       </div>
       <button type="button" onClick={handleClose}>
         <CloseSVG className="fill-black" />
