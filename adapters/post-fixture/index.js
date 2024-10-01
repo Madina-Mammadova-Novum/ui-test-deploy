0250ac0b445aee879680ceb52a87efd2d36ff0e0
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2024-10-01 13:40:39 +0300

    fix(961): remove unnecessary file name logic

--- adapters/post-fixture/index.js
+++ adapters/post-fixture/index.js
@@ -271,10 +271,19 @@ export const postFixtureDetailsAdapter = ({ data }) => {
 
 const postFixtureDocumentsTabRowDataAdapter = ({ data, index }) => {
   if (!data) return [];
-  const { id, title, comments, name, extention, size, createdAt, status, url, deleted: isDocumentDeleted } = data;
+  const {
+    id,
+    title,
+    comments,
+    name: fileName,
+    extention,
+    size,
+    createdAt,
+    status,
+    url,
+    deleted: isDocumentDeleted,
+  } = data;
   const revokeDeletionForbidden = status === 'Active';
-
-  const fileName = name.split('.').pop() === extention ? name : `${name}${extention}`;
 
   return [
     {
@@ -307,7 +316,7 @@ const postFixtureDocumentsTabRowDataAdapter = ({ data, index }) => {
     },
     {
       id,
-      value: name,
+      value: fileName,
       disabled: isDocumentDeleted,
     },
     {
