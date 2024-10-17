5a7a93425dd794a704118850654c9b6cfe1b67fb
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2024-10-17 09:15:20 +0300

    fix: update tailwind classnames for better usage

--- components/modules/Header/MaintenanceHeader/index.js
+++ components/modules/Header/MaintenanceHeader/index.js
@@ -7,7 +7,7 @@ export default async function MaintenanceHeader() {
   return (
     <header className="absolute z-10 w-full">
       <div className="container mx-auto max-w-[1258px] px-6 3md:px-14">
-        <div className="align-center flex justify-between border-b border-white/10 py-2.5">
+        <div className="flex items-center justify-between border-b border-white/10 py-2.5">
           <Logo className="hidden fill-white md:block" />
           <SmallLogo className="fill-white md:hidden" />
         </div>
--- components/modules/Header/PageHeader/index.js
+++ components/modules/Header/PageHeader/index.js
@@ -52,7 +52,7 @@ export default async function PageHeader() {
   return (
     <header className="absolute z-10 w-full">
       <div className="container mx-auto max-w-[1258px] px-6 3md:px-14">
-        <div className="align-center flex justify-between border-b border-white/10 py-2.5">
+        <div className="flex items-center justify-between border-b border-white/10 py-2.5">
           <NextLink href="/">
             <Logo className="hidden fill-white md:block" />
             <SmallLogo className="fill-white md:hidden" />
