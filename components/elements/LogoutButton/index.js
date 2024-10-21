e151dcc6c663e8e397f846a8be4a2c8929767607
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2024-10-21 16:42:23 +0300

    refactor: add usehandlelogout custom hook

--- components/elements/LogoutButton/index.js
+++ components/elements/LogoutButton/index.js
@@ -1,34 +1,18 @@
 'use client';
 
-import { useDispatch } from 'react-redux';
-
-import { useRouter } from 'next/navigation';
-
 import { LogoutButtonPropTypes } from '@/lib/types';
 
 import { Button } from '@/elements';
-import { ROUTES } from '@/lib';
-import { clearSession } from '@/store/entities/auth/slice';
-import { resetChat } from '@/store/entities/chat/slice';
-import { resetNotificationData } from '@/store/entities/notifications/slice';
+import { useHandleLogout } from '@/utils/hooks';
 
 const LogoutButton = ({ text = 'Log out', variant = 'tertiary', className = '!border-none', icon }) => {
-  const router = useRouter();
-  const dispatch = useDispatch();
-
-  const handleSignOut = () => {
-    router.replace(ROUTES.LOGIN);
-    dispatch(clearSession());
-    dispatch(resetNotificationData());
-    dispatch(resetChat());
-    router.refresh();
-  };
+  const handleLogout = useHandleLogout();
 
   return (
     <Button
       customStyles={className}
       buttonProps={{ text, variant, icon: { before: icon }, size: 'large' }}
-      onClick={handleSignOut}
+      onClick={handleLogout}
     />
   );
 };
