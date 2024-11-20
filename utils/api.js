e94b4a9d6169c86fabbcd4d5f78b235e121ce842
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2024-11-20 11:57:55 +0300

    feat(1247): update ip on header

--- utils/api.js
+++ utils/api.js
@@ -10,7 +10,12 @@ import { api } from '@/lib/axios';
 
 export const apiHandler = async (options) => {
   try {
-    const response = await api.request(apiOptionsAdapter(options));
+    const headers = {
+      ...options.headers,
+      'x-forwarded-for': options.forwardedFor || options.ip,
+    };
+
+    const response = await api.request(apiOptionsAdapter({ ...options, headers }));
 
     return apiSuccessAdapter(successResponseAdapter(response));
   } catch (error) {
@@ -19,7 +24,23 @@ export const apiHandler = async (options) => {
 };
 
 export const responseHandler = async ({ req, res, dataAdapter, ...rest }) => {
-  const response = await apiHandler({ body: req.body, options: req.options, ...rest });
+  const ip =
+    req.headers['x-real-ip'] ||
+    req.headers['x-forwarded-for']?.split(',')[0] ||
+    req.headers['x-client-ip'] ||
+    req.socket?.remoteAddress ||
+    'Unknown';
+
+  const response = await apiHandler({
+    body: req.body,
+    options: req.options,
+    headers: {
+      ...req.headers,
+      'x-forwarded-for': ip,
+    },
+    ...rest,
+  });
+
   const data = await dataAdapter({ data: response.data });
 
   return res.status(response.status).json({ ...response, ...responseAdapter(data) });
