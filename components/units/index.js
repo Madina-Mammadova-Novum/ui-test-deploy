760567e2c173163aa3bcc029c717825e6dfabfbb
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2024-12-18 16:26:03 +0300

    refactor: add nested checkboxlist component
    @description Reusable nested checkbox component for tree-like data structures

--- components/units/index.js
+++ components/units/index.js
@@ -67,6 +67,7 @@ import ImoNotFound from '@/units/ImoNotFound';
 import LegalNavigation from '@/units/LegalNavigation';
 import ModalHeader from '@/units/ModalHeader';
 import ModalWindow from '@/units/ModalWindow';
+import NestedCheckboxList from '@/units/NestedCheckboxList';
 import NegotiatingChartererInformation from '@/units/NegotiatingChartererInformation';
 import NegotiatingTankerInformation from '@/units/NegotiatingTankerInformation';
 import Notes from '@/units/Notes';
@@ -219,4 +220,5 @@ export {
   FavoriteSearchForm,
   FavoriteSearchList,
   ConfirmModal,
+  NestedCheckboxList,
 };
