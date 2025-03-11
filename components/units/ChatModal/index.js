1dcff58b25b1a4057806d9c514cdfac85c7f1e31
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2025-03-11 17:27:51 +0300

    refactor(1514): add responsive logic into auth chat

--- components/units/ChatModal/index.js
+++ components/units/ChatModal/index.js
@@ -7,7 +7,7 @@ import { ChatModalHeader } from '@/units';
 const ChatModal = ({ isOpened, useCollapse, loading, children, onClose, onCollapse }) => {
   return (
     isOpened && (
-      <div className="fixed bottom-6 right-24 z-10 h-auto w-[360px] rounded-base bg-white shadow-xmd">
+      <div className="z-100 fixed bottom-6 right-24 h-auto w-[360px] rounded-base bg-white shadow-xmd md:z-10">
         <ChatModalHeader useCollapse={useCollapse} loading={loading} onClose={onClose} onCollapse={onCollapse} />
         {children}
       </div>
