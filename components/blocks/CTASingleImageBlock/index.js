eee281c133716a3fab35037241700517534c387f
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2025-03-21 18:52:12 +0300

    feat(1578): add videoplayer into single image block

--- components/blocks/CTASingleImageBlock/index.js
+++ components/blocks/CTASingleImageBlock/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import { linkPropTypes, mediaPropTypes } from '@/lib/types';
 
-import { LinkAsButton, NextImage, Title } from '@/elements';
+import { LinkAsButton, NextImage, Title, VideoPlayer } from '@/elements';
 import { getStrapiMedia } from '@/utils';
 
 const CTASingleImageBlock = ({ title, shortDescription, coverImage, button }) => {
@@ -22,27 +22,42 @@ const CTASingleImageBlock = ({ title, shortDescription, coverImage, button }) =>
           />
         </div>
       )}
-      <div className="mx-auto max-w-[500px] rounded-base bg-white py-8">
-        {title && (
-          <Title level="1" className="mb-2.5 text-center text-black">
-            {title}
-          </Title>
-        )}
-        <div className="mx-auto max-w-[300px]">
-          {shortDescription && <p className="mb-5 text-center text-xsm text-black">{shortDescription}</p>}
-          {button && (
-            <LinkAsButton
-              href={button.path}
-              buttonProps={{
-                variant: 'primary',
-                size: 'large',
-              }}
-              customStyles="max-w-[115px] mx-auto"
-              target={button.target}
-            >
-              {button.label}
-            </LinkAsButton>
+
+      <div className="mx-auto max-w-[690px] rounded-base">
+        <div className="mb-10 w-full overflow-hidden rounded-lg shadow-xl">
+          <VideoPlayer
+            src="/videos/intro.mp4"
+            controls
+            autoPlay={false}
+            loop={false}
+            muted={false}
+            preload="auto"
+            className="w-full"
+          />
+        </div>
+
+        <div className="mx-auto max-w-[500px] rounded-base bg-white py-8">
+          {title && (
+            <Title level="1" className="mb-2.5 text-center text-black">
+              {title}
+            </Title>
           )}
+          <div className="mx-auto max-w-[300px]">
+            {shortDescription && <p className="mb-5 text-center text-xsm text-black">{shortDescription}</p>}
+            {button && (
+              <LinkAsButton
+                href={button.path}
+                buttonProps={{
+                  variant: 'primary',
+                  size: 'large',
+                }}
+                customStyles="max-w-[115px] mx-auto"
+                target={button.target}
+              >
+                {button.label}
+              </LinkAsButton>
+            )}
+          </div>
         </div>
       </div>
     </section>
