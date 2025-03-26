4b49cec29d488edbd49a45938437c1b5747c5422
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2025-03-26 12:37:11 +0300

    ci: remove logs and default value on config

--- components/common/NewRelicBrowser/index.js
+++ components/common/NewRelicBrowser/index.js
@@ -3,28 +3,13 @@
 import { useEffect } from 'react';
 
 const NewRelicBrowser = () => {
-  const test = process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_LICENSE_KEY;
-  const test2 = process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_APP_ID;
-  const test3 = process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_AGENT_ID;
-  const test4 = process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_TRUST_KEY;
-  const test5 = process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_ACCOUNT_ID;
-  const backendApiUrl = process.env.BACKEND_API_URL;
-  // eslint-disable-next-line no-console
-  console.log({ test, test2, test3, test4, test5, backendApiUrl });
-
   useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log('NewRelicBrowser');
     // Only run in browser
     if (typeof window !== 'undefined') {
       // Correctly import the New Relic browser agent
       import('@newrelic/browser-agent/loaders/browser-agent')
         .then((module) => {
-          // eslint-disable-next-line no-console
-          console.log('New Relic Browser module loaded');
           try {
-            // eslint-disable-next-line no-console
-            console.log('New Relic Browser module loaded 2');
             const { BrowserAgent } = module;
 
             if (!BrowserAgent) {
@@ -56,22 +41,20 @@ const NewRelicBrowser = () => {
               info: {
                 beacon: 'bam.eu01.nr-data.net',
                 errorBeacon: 'bam.eu01.nr-data.net',
-                licenseKey: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_LICENSE_KEY || 'NRJS-781ca6a265162a4f947',
-                applicationID: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_APP_ID || '538699265',
-                agentID: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_AGENT_ID || '538699265',
-                trustKey: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_TRUST_KEY || '4589435',
+                licenseKey: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_LICENSE_KEY,
+                applicationID: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_APP_ID,
+                agentID: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_AGENT_ID,
+                trustKey: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_TRUST_KEY,
                 sa: 1,
               },
               loader_config: {
-                accountID: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_ACCOUNT_ID || '4589435',
-                trustKey: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_TRUST_KEY || '4589435',
-                agentID: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_AGENT_ID || '538699265',
-                licenseKey: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_LICENSE_KEY || 'NRJS-781ca6a265162a4f947',
-                applicationID: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_APP_ID || '538699265',
+                accountID: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_ACCOUNT_ID,
+                trustKey: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_TRUST_KEY,
+                agentID: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_AGENT_ID,
+                licenseKey: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_LICENSE_KEY,
+                applicationID: process.env.NEXT_PUBLIC_NEW_RELIC_BROWSER_APP_ID,
               },
             });
-            // eslint-disable-next-line no-console
-            console.log('New Relic Browser initialized successfully: ', agent);
           } catch (error) {
             console.error('Failed to initialize New Relic Browser:', error);
           }
