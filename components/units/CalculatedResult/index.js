448ac1a6d0fa83394bd2b03a84bab6776208aec3
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2025-04-08 14:48:55 +0300

    refactor(1679): update unit on freight estimation with refactor

--- components/units/CalculatedResult/index.js
+++ components/units/CalculatedResult/index.js
@@ -1,17 +1,37 @@
 'use client';
 
+import PropTypes from 'prop-types';
+
 import { CalculatedResultPropTypes } from '@/lib/types';
 
 import { TextWithLabel, Title } from '@/elements';
 import { formattedDay, formattedNumber } from '@/utils/helpers';
 import { toolsCalculatorOptions } from '@/utils/mock';
 
+const ResultItem = ({ label, text }) => (
+  <TextWithLabel
+    text={text}
+    label={label}
+    customStyles="!flex-col !items-start [&>label]:!text-[10px] [&>p]:!ml-0"
+    textGroupStyle="!mx-0 !ml-0"
+  />
+);
+
+ResultItem.propTypes = {
+  label: PropTypes.string.isRequired,
+  text: PropTypes.string,
+};
+
 const CalculatedResult = ({ result, value }) => {
   const isFreight = value === toolsCalculatorOptions[0]?.value;
   const firstValue = formattedNumber(result?.resultOne);
   const secondValue = formattedNumber(result?.resultTwo);
-  const secondTextWithValue = `${formattedDay(secondValue)} Days`;
+
+  // Format display values based on calculation type
   const distanceValue = firstValue ? `${firstValue} nm` : '';
+  const durationValue = secondValue ? `${formattedDay(secondValue)} days` : '';
+  const freightValue = secondValue ? `$${secondValue}` : '';
+  const costPerTonValue = firstValue ? `$${firstValue}` : '';
 
   return (
     <div className="absolute bottom-2 left-2 z-[500] w-[250px] rounded-md bg-[rgba(255,255,255,0.8)] p-5 font-inter-sans">
@@ -19,33 +39,13 @@ const CalculatedResult = ({ result, value }) => {
       <div className="mt-2.5 flex gap-x-2.5">
         {isFreight ? (
           <>
-            <TextWithLabel
-              text={secondValue}
-              label="Total freight"
-              customStyles="!flex-col !items-start [&>label]:!text-[10px] [&>p]:!ml-0"
-              textGroupStyle="!mx-0 !ml-0"
-            />
-            <TextWithLabel
-              text={firstValue}
-              label="Cost per ton"
-              customStyles="!flex-col !items-start [&>label]:!text-[10px] [&>p]:!ml-0"
-              textGroupStyle="!mx-0 !ml-0"
-            />
+            <ResultItem label="Total freight" text={freightValue} />
+            <ResultItem label="Cost per ton" text={costPerTonValue} />
           </>
         ) : (
           <>
-            <TextWithLabel
-              text={distanceValue}
-              label="Distance"
-              customStyles="!flex-col !items-start [&>label]:!text-[10px] [&>p]:!ml-0"
-              textGroupStyle="!mx-0 !ml-0"
-            />
-            <TextWithLabel
-              text={secondValue && secondTextWithValue}
-              label="Duration"
-              customStyles="!flex-col !items-start [&>label]:!text-[10px] [&>p]:!ml-0"
-              textGroupStyle="!mx-0 !ml-0"
-            />
+            <ResultItem label="Distance" text={distanceValue} />
+            <ResultItem label="Duration" text={durationValue} />
           </>
         )}
       </div>
