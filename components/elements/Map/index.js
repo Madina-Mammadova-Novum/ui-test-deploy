03a1009217a95da28d81258f9c000b9f166031cf
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2025-05-06 14:59:03 +0300

    refactor: add loader for map

--- components/elements/Map/index.js
+++ components/elements/Map/index.js
@@ -1,20 +1,37 @@
+'use client';
+
+import { useState } from 'react';
+
 import classNames from 'classnames';
 
 import { MapPropTypes } from '@/lib/types';
 
 const Map = ({ title, embedMap, className }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const src = embedMap.match(/src="([^"]+)"/)[1];
+
   return (
-    <iframe
-      title={title}
-      src={src}
-      width="536"
-      height="400"
-      allowFullScreen=""
-      loading="lazy"
-      referrerPolicy="no-referrer-when-downgrade"
-      className={classNames('h-[400px] w-full rounded-base', className)}
-    />
+    <div className="relative w-full md:w-auto">
+      {isLoading && (
+        <div className="absolute inset-0 flex items-center justify-center rounded-base bg-gray-100">
+          <div className="text-center">
+            <div className="mx-auto mb-2 h-10 w-10 animate-spin rounded-full border-b-2 border-black" />
+            <p className="mt-4 text-xsm text-black">Loading map...</p>
+          </div>
+        </div>
+      )}
+      <iframe
+        title={title}
+        src={src}
+        width="536"
+        height="400"
+        allowFullScreen=""
+        loading="eager"
+        referrerPolicy="no-referrer-when-downgrade"
+        className={classNames('h-[400px] w-full rounded-base', className)}
+        onLoad={() => setIsLoading(false)}
+      />
+    </div>
   );
 };
 
