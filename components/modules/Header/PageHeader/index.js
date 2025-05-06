283760729d2fd52dba1ac7a8823d635bdc964aea
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2025-05-06 13:53:05 +0300

    fix: update header logo and spacing

--- components/modules/Header/PageHeader/index.js
+++ components/modules/Header/PageHeader/index.js
@@ -54,8 +54,8 @@ export default async function PageHeader() {
 
   return (
     <header className="absolute z-10 w-full">
-      <div className="container mx-auto max-w-[1258px] px-6 3md:px-14">
-        <div className="flex items-center justify-between border-b border-white/10 py-2.5">
+      <div className="container mx-auto max-w-[1258px] px-4 md:px-8 3md:px-14">
+        <div className="flex items-center justify-between border-b border-white/10 py-6 3md:py-5">
           <NextLink href="/">
             <Logo className="hidden fill-white md:block" />
             <SmallLogo className="fill-white md:hidden" />
