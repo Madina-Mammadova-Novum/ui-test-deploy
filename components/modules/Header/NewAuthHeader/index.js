5f1ec8dd1acd13d140f6b45729715934411ccff6
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2025-05-21 12:19:25 +0300

    feat(1848): add in app checker as provider
    header and footer removed from mobile view from app

--- components/modules/Header/NewAuthHeader/index.js
+++ components/modules/Header/NewAuthHeader/index.js
@@ -1,13 +1,21 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
 import { AuthBasePropTypes } from '@/lib/types';
 
 import { NewNavbar } from '@/modules';
 
+const InAppChecker = dynamic(() => import('@/common/InAppChecker'), { ssr: false });
+
 const NewAuthHeader = ({ navigation }) => {
   const { placeholder, cta, contrasted, path } = navigation;
   return (
-    <header className="sticky left-0 top-0 z-10 mx-auto flex w-full max-w-[1152px] px-4 md:px-8 xl:px-0">
-      <NewNavbar placeholder={placeholder} cta={cta} contrasted={contrasted} path={path} />
-    </header>
+    <InAppChecker>
+      <header className="sticky left-0 top-0 z-10 mx-auto flex w-full max-w-[1152px] px-4 md:px-8 xl:px-0">
+        <NewNavbar placeholder={placeholder} cta={cta} contrasted={contrasted} path={path} />
+      </header>
+    </InAppChecker>
   );
 };
 
