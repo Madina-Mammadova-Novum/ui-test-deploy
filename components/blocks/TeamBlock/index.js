a45ead4f86b8625d1009caff52ff3e51271edca5
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2025-05-30 14:32:28 +0300

    fix(1600): adjust height of team card

--- components/blocks/TeamBlock/index.js
+++ components/blocks/TeamBlock/index.js
@@ -42,7 +42,7 @@ const TeamBlock = ({ title, subTitle, shortDescription, members }) => {
   );
 
   const printMember = ({ fullName, content, position, coverImage, socialLinks }) => (
-    <SwiperSlide key={makeId()} className="flex h-auto px-2">
+    <SwiperSlide key={makeId()} className="flex px-2">
       <div className="flex w-full max-w-full flex-col gap-y-6 rounded-base bg-white p-8 text-black shadow-2xmd">
         <div className="flex items-center gap-4">
           {coverImage && (
