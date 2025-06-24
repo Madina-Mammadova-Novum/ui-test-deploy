772a22959726ddbe557005145dde4834468fa786
Deniz Cansever <67337875+DenizCansever@users.noreply.github.com>
2025-06-24 18:12:25 +0300

    fix: resolve small ui bugs

--- components/modules/Footer/AccountFooter/index.js
+++ components/modules/Footer/AccountFooter/index.js
@@ -11,7 +11,7 @@ const InAppChecker = dynamic(() => import('@/common/InAppChecker'), { ssr: false
 export default function AccountFooter({ socials, legal }) {
   return (
     <InAppChecker>
-      <footer className="mt-2 flex flex-col items-start gap-2.5 bg-white px-5 py-2 text-xs-sm shadow-xmd md:grid md:grid-cols-6 md:items-center md:py-4 3md:grid-cols-3">
+      <footer className="mt-[1.875rem] flex flex-col items-start gap-2.5 bg-white px-5 py-2 text-xs-sm shadow-xmd md:grid md:grid-cols-6 md:items-center md:py-4 3md:grid-cols-3">
         <SocialNetworks data={socials} />
         <div className="flex w-full flex-col items-center gap-1 text-center md:col-span-4 md:gap-2 3md:col-span-1">
           <LegalNavigation data={legal} />
